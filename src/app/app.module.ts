e453d15dab3cbd69e88d286aa6edb6642f7cf510
shrinivassab <shrinivassab@gmail.com>
2022-10-27 23:11:05 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,18 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SideBarComponent } from './side-bar/side-bar.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { FooterComponent } from './footer/footer.component';
-import { TabMenuModule } from 'primeng/tabmenu';
-import { AppRoutingModule } from './app-routing/app-routing.module';
-import { ExperienceComponent } from './dashboard/experience/experience.component';
-import { AboutComponent } from './dashboard/about/about.component';
-import { ProjectsComponent } from './dashboard/projects/projects.component';
-import { HomeComponent } from './dashboard/home/home.component';
-import { SkillsComponent } from './dashboard/skills/skills.component';
-import { ContactComponent } from './dashboard/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SideBarComponent,
-    TopBarComponent,
-    DashboardComponent,
-    FooterComponent,
-    ExperienceComponent,
-    AboutComponent,
-    ProjectsComponent,
-    HomeComponent,
-    SkillsComponent,
-    ContactComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    TabMenuModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
